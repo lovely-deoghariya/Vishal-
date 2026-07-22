73b2b3310f00f6c2c3c0672490932c65b85e3ee6
Lovely Deoghariya <lovelydeoghariya@gmail.com>
2026-07-22 21:42:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function nextPage() {
+    document.body.innerHTML = `
+    <div class="hearts"></div>
+
+    <section class="page">
+
+        <h1>💌 Dear Sweetheart ❤️</h1>
+
+        <p>
+        Aapke liye maine ek chhota sa surprise tayyar kiya hai.
+        Is website ka har page sirf aapko smile dene ke liye banaya gaya hai. 🥹❤️
+        </p>
+
+        <p>
+        Taiyaar hain meri chhoti si duniya dekhne ke liye?
+        </p>
+
+        <button onclick="alert('Part 2 me 7 Shayari shuru hongi ❤️')">
+        Continue ➜
+        </button>
+
+    </section>
+    `;
+}
